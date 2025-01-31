28db63e6672872e694a14597d69056f289af0828
kachofugetsu09 <2494946808@qq.com>
2025-01-31 18:11:45 +0800

    套餐分页查询相关业务

--- sky-server/src/main/java/com/sky/controller/admin/SetmealControllder.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealControllder.java
@@ -1,15 +1,14 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.sky.service.SetmealService;
 @RestController
 @RequestMapping("/admin/setmeal")
@@ -23,5 +22,11 @@ public class SetmealControllder {
     public Result save(@RequestBody SetmealDTO setmealDTO){
         SetmealService.saveWithDish(setmealDTO);
         return Result.success();
+    }
+    @GetMapping("/page")
+    @ApiOperation("套餐分页查询")
+    public Result<PageResult> page(SetmealPageQueryDTO setmealDTO){
+        PageResult pageResult = SetmealService.pageQuery(setmealDTO);
+        return Result.success(pageResult);
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,12 +1,17 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.mapper.DishMapper;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,4 +47,20 @@ public class SetmealServiceImpl implements SetmealService {
         setmealDishMapper.insertBatch(setmealDishList);
 
     }
+
+    /**
+     * @param setmealDTO
+     * @return
+     */
+    @Override
+    public PageResult pageQuery(SetmealPageQueryDTO setmealDTO) {
+        PageHelper.startPage(setmealDTO.getPage(),setmealDTO.getPageSize());
+        Page<SetmealVO> page = setmealMapper.pageQuery(setmealDTO);
+        long total = page.getTotal();
+        List<SetmealVO> records = page.getResult();
+        return new PageResult(total,records);
+    }
+
+
+
 }
